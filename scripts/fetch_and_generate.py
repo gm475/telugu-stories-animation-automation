dd7fcff3ee662a6d4c98592fa76c72e468656c39
gm475 <gm475.digital@gmail.com>
2025-01-06 00:24:03 +0530

    update

--- scripts/fetch_and_generate.py
+++ scripts/fetch_and_generate.py
@@ -1,10 +1,18 @@
 from pytrends.request import TrendReq
 
-def fetch_trending_topics():
+def fetch_indian_kids_animation_topic():
     pytrends = TrendReq(hl="en-US", tz=360)
     trending_searches = pytrends.trending_searches(pn="india")
-    return trending_searches[0][0]  # Return top trending topic
+    
+    # List of keywords related to Indian animation and languages
+    animation_keywords = ["animation", "cartoon", "2D", "3D", "kids story", "children", "telugu", "hindi", "short films", "animated series"]
+    
+    for topic in trending_searches[0]:
+        if any(keyword.lower() in topic.lower() for keyword in animation_keywords):
+            return topic  # Return the first matching topic
 
-topic = fetch_trending_topics()
+    return "No relevant Indian kids' animation topic found"  # If no match found
 
-print(f"Top trending topic in India: {topic}")
+topic = fetch_indian_kids_animation_topic()
+
+print(f"Top trending Indian kids' animation topic: {topic}")
