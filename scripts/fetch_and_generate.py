2548c34213ac62751790b31fafa5743843824405
gm475 <gm475.digital@gmail.com>
2025-01-06 00:25:21 +0530

    update

--- scripts/fetch_and_generate.py
+++ scripts/fetch_and_generate.py
@@ -4,14 +4,18 @@ def fetch_indian_kids_animation_topic():
     pytrends = TrendReq(hl="en-US", tz=360)
     trending_searches = pytrends.trending_searches(pn="india")
     
-    # List of keywords related to Indian animation and languages
-    animation_keywords = ["animation", "cartoon", "2D", "3D", "kids story", "children", "telugu", "hindi", "short films", "animated series"]
+    # Focus on kids' content, animation, Telugu/Hindi
+    animation_keywords = [
+        "kids animation", "2D animation", "3D animation", "cartoon", "children story", 
+        "Chhota Bheem", "Bal Ganesh", "Doraemon", "Krishna", "Motu Patlu", "animated series",
+        "telugu animation", "hindi animation", "kids cartoon"
+    ]
     
     for topic in trending_searches[0]:
         if any(keyword.lower() in topic.lower() for keyword in animation_keywords):
             return topic  # Return the first matching topic
 
-    return "No relevant Indian kids' animation topic found"  # If no match found
+    return "No relevant Indian kids' animation topic found"
 
 topic = fetch_indian_kids_animation_topic()
 
